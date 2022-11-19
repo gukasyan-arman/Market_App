2bf92680c3f46ba8f0a1c806198a018e7aa3e4c1
gukasyan-arman <gukasian.a.a@gmail.com>
2022-11-19 17:11:55 +0400

    filter Block

--- app/src/main/java/com/example/testapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/testapp/ui/home/HomeFragment.kt
@@ -5,17 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.testapp.R
 import com.example.testapp.adapters.BestSellerAdapter
 import com.example.testapp.adapters.ViewPagerAdapter
 import com.example.testapp.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
 
     private val homeViewModel by viewModels<HomeViewModel>()
     lateinit var viewPagerAdapter: ViewPagerAdapter
@@ -46,6 +50,27 @@ class HomeFragment : Fragment() {
             }
         }
 
+        initPhonesDropdown()
+        initPricesDropdown()
+        initSizesDropdown()
+
+        binding.filter.setOnClickListener {
+            if (binding.filterBlock.visibility == View.GONE) {
+                binding.filterBlock.visibility = View.VISIBLE
+            } else {
+                binding.filterBlock.visibility = View.GONE
+            }
+        }
+
+        binding.filterCancelBtn.setOnClickListener {
+            binding.filterBlock.visibility = View.GONE
+        }
+
+        binding.filterDoneBtn.setOnClickListener {
+            binding.filterBlock.visibility = View.GONE
+            Toast.makeText(requireContext(), "Filters applied", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun initBestSellerAdapter() {
@@ -57,6 +82,24 @@ class HomeFragment : Fragment() {
     private fun initViewPagerAdapter() {
         viewPagerAdapter = ViewPagerAdapter()
         binding.viewPager.adapter = viewPagerAdapter
+    }
+
+    private fun initPhonesDropdown() {
+        val phones = resources.getStringArray(R.array.phones)
+        val phonesAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, phones)
+        binding.autoCompletePhone.setAdapter(phonesAdapter)
+    }
+
+    private fun initPricesDropdown() {
+        val prices = resources.getStringArray(R.array.prices)
+        val pricesAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, prices)
+        binding.autoCompletePrices.setAdapter(pricesAdapter)
+    }
+
+    private fun initSizesDropdown() {
+        val sizes = resources.getStringArray(R.array.sizes)
+        val sizesAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, sizes)
+        binding.autoCompleteSizes.setAdapter(sizesAdapter)
     }
 
 }
